567768a58e221d46102be344f8fe4cf91af9dd4b
Redolyr <hyresann1221@hotmail.co.jp>
2016-11-23 18:33:18 +0900

    Delete DocumentBaseArray.java

--- src/main/java/SupplyPower/documentSystems/DocumentBaseArray.java
+++
@@ -1,9 +0,0 @@
-package SupplyPower.documentSystems;
-
-/**
- * Created by redolyr on 2016/08/10.
- */
-public abstract class DocumentBaseArray extends DocumentBase {
-
-    public abstract int length();
-}
